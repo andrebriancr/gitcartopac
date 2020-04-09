254565a04cc2ff1b68308e006f99f298bfc07ea8
= <parteaga@cartonesdelpacifico.com.pe>
2020-04-08 19:22:51 -0500

    eliminamos despedida y prueba

--- js/app.js
+++ js/app.js
@@ -2,10 +2,3 @@ console.log("Server iniciado");
 
 console.log("Bienvenido");
 console.log("Prueba");
-
-console.log("Despedida");
-console.log("Prueba");
-console.log("Prueba");
-console.log("Prueba");
-console.log("Prueba");
-console.log("Prueba");
