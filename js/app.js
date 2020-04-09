dadca7ccc5e29286d5ac7fc8026a329458f40eb4
= <parteaga@cartonesdelpacifico.com.pe>
2020-04-08 19:41:29 -0500

    Grabar

--- js/app.js
+++ js/app.js
@@ -2,11 +2,3 @@ console.log("Server iniciado");
 
 console.log("Bienvenido");
 console.log("Prueba");
-<<<<<<< HEAD
-
-console.log("Despedida");
-console.log("Despedida");
-console.log("Despedida");
-console.log("Despedida");
-=======
->>>>>>> errores
